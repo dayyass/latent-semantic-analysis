20c6761ec40de47ed26b1095ec9aaa9a2381dbdd
Dani El-Ayyass <dayyass@yandex.ru>
2021-09-26 17:55:41 +0300

    add reproducibility

--- latent-semantic-analysis/utils.py
+++ latent-semantic-analysis/utils.py
@@ -1,4 +1,6 @@
 from argparse import ArgumentParser
+import random
+import numpy as np
 
 
 def get_argparse() -> ArgumentParser:
@@ -17,3 +19,13 @@ def get_argparse() -> ArgumentParser:
     )
 
     return parser
+
+
+def set_seed(seed: int) -> None:
+    """
+    Set seed for reproducibility.
+    :param int seed: seed.
+    """
+
+    random.seed(seed)
+    np.random.seed(seed)
