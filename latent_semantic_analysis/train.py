ee26ede3847d41febb1d4e3701f915e79307c823
Dani El-Ayyass <dayyass@yandex.ru>
2021-09-27 22:09:14 +0300

    refactor train

--- latent_semantic_analysis/train.py
+++ latent_semantic_analysis/train.py
@@ -1,16 +1,13 @@
 import logging
 from typing import Any, Dict
 
+from sklearn.decomposition import TruncatedSVD
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
-from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import LabelEncoder
 
 from .data import load_data
 from .save import save_model
-from .utils import close_logger, get_grid_search_params, prepare_dict_to_print, set_seed
+from .utils import close_logger, set_seed
 
 
 def _train(
@@ -18,7 +15,7 @@ def _train(
     logger: logging.Logger,
 ) -> None:
     """
-    Function to train baseline model.
+    Function to train LSA model.
     :param Dict[str, Any] config: config.
     :param logging.Logger logger: logger.
     """
@@ -33,25 +30,16 @@ def _train(
     # load data
     logger.info("Loading data...")
 
-    X_train, X_valid, y_train, y_valid = load_data(config)
+    data = load_data(config)
 
-    logger.info(f"Train dataset size: {X_train.shape[0]}")
-    logger.info(f"Valid dataset size: {X_valid.shape[0]}")
-
-    # label encoder
-    le = LabelEncoder()
-    y_train = le.fit_transform(y_train)
-    y_valid = le.transform(y_valid)
-
-    target_names = [str(cls) for cls in le.classes_.tolist()]
-    target_names_mapping = {i: cls for i, cls in enumerate(target_names)}
+    logger.info(f"Dataset size: {data.shape[0]}")
 
     # tf-idf
     vectorizer = TfidfVectorizer(**config["tf-idf"])
 
-    # logreg
-    clf = LogisticRegression(
-        **config["logreg"],
+    # svd
+    svd = TruncatedSVD(
+        **config["svd"],
         random_state=config["seed"],
     )
 
@@ -59,61 +47,44 @@ def _train(
     pipe = Pipeline(
         [
             ("tf-idf", vectorizer),
-            ("logreg", clf),
+            ("svd", svd),
         ],
-        verbose=False if config["grid-search"]["do_grid_search"] else True,
+        verbose=True,  # hardcoded
     )
 
-    if config["grid-search"]["do_grid_search"]:
-        logger.info("Finding best hyper-parameters...")
+    logger.info("Fitting LSA model...")
 
-        grid_search_params = get_grid_search_params(
-            config["grid-search"]["grid_search_params_path"]
-        )
-        grid = GridSearchCV(pipe, **grid_search_params)
-        grid.fit(X_train, y_train)
-
-        pipe = grid.best_estimator_
-
-        logger.info(
-            f"Best hyper-parameters:\n{prepare_dict_to_print(grid.best_params_)}"
-        )
-
-    else:
-        logger.info("Fitting TF-IDF + LogReg model...")
-
-        pipe.fit(X_train, y_train)
+    pipe.fit(data)
 
     logger.info("Done!")
     logger.info(f"TF-IDF number of features: {len(pipe['tf-idf'].vocabulary_)}")
 
-    # metrics
-    logger.info("Calculating metrics...")
+    # # metrics
+    # logger.info("Calculating metrics...")
 
-    y_pred_train = pipe.predict(X_train)
-    classification_report_train = classification_report(
-        y_true=y_train,
-        y_pred=y_pred_train,
-        target_names=target_names,
-    )
+    # y_pred_train = pipe.predict(X_train)
+    # classification_report_train = classification_report(
+    #     y_true=y_train,
+    #     y_pred=y_pred_train,
+    #     target_names=target_names,
+    # )
 
-    logger.info(f"Train classification report:\n\n{classification_report_train}")
+    # logger.info(f"Train classification report:\n\n{classification_report_train}")
 
-    y_pred_valid = pipe.predict(X_valid)
-    classification_report_valid = classification_report(
-        y_true=y_valid,
-        y_pred=y_pred_valid,
-        target_names=target_names,
-    )
+    # y_pred_valid = pipe.predict(X_valid)
+    # classification_report_valid = classification_report(
+    #     y_true=y_valid,
+    #     y_pred=y_pred_valid,
+    #     target_names=target_names,
+    # )
 
-    logger.info(f"Valid classification report:\n\n{classification_report_valid}")
+    # logger.info(f"Valid classification report:\n\n{classification_report_valid}")
 
     # save model
     logger.info("Saving the model...")
 
     save_model(
         pipe=pipe,
-        target_names_mapping=target_names_mapping,
         config=config,
     )
 
