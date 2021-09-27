71e3f911bc44192fbd4be8b7c2ecccb6d06e9002
Dani El-Ayyass <dayyass@yandex.ru>
2021-09-27 21:39:23 +0300

    add logging

--- latent-semantic-analysis/utils.py
+++ latent-semantic-analysis/utils.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 import random
 import numpy as np
+import logging
 
 
 def get_argparse() -> ArgumentParser:
@@ -19,6 +20,50 @@ def get_argparse() -> ArgumentParser:
     )
 
     return parser
+
+
+def get_logger(path_to_logfile: str) -> logging.Logger:
+    """
+    Get logger.
+    :param str path_to_logfile: path to logfile.
+    :return: logger.
+    :rtype: logging.Logger
+    """
+
+    logger = logging.getLogger("text-clf-train")
+    logger.setLevel(logging.INFO)
+
+    # create handlers
+    stream_handler = logging.StreamHandler(sys.stdout)
+    file_handler = logging.FileHandler(path_to_logfile)
+    stream_handler.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+
+    # create formatters and add it to handlers
+    stream_format = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+    file_format = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    stream_handler.setFormatter(stream_format)
+    file_handler.setFormatter(file_format)
+
+    # add handlers to the logger
+    logger.addHandler(stream_handler)
+    logger.addHandler(file_handler)
+
+    return logger
+
+
+def close_logger(logger: logging.Logger) -> None:
+    """
+    Close logger.
+    Source: https://stackoverflow.com/questions/15435652/python-does-not-release-filehandles-to-logfile
+    :param logging.Logger logger: logger.
+    """
+
+    for handler in logger.handlers[:]:
+        handler.close()
+        logger.removeHandler(handler)
 
 
 def set_seed(seed: int) -> None:
